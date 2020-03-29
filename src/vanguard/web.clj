686a10bc456797f952d2ba1f71f962c169b3255c
Taso Lyristis <taso@lyrisoft.com>
2020-03-29 16:27:00 -0700

    type hints

--- src/vanguard/web.clj
+++ src/vanguard/web.clj
@@ -2,20 +2,21 @@
   (:import
     (org.openqa.selenium.chrome ChromeDriver)
     (org.openqa.selenium By)
-    (org.openqa.selenium.support.ui WebDriverWait ExpectedConditions)))
+    (org.openqa.selenium.support.ui WebDriverWait ExpectedConditions)
+    (org.openqa.selenium.remote RemoteWebElement RemoteWebDriver)))
 
 
 (System/setProperty "webdriver.chrome.driver" "/Users/taso/Downloads/chromedriver")
 
 
 (defn populate
-  [element content]
+  [^RemoteWebElement element content]
   (.clear element)
   (.sendKeys element (into-array java.lang.CharSequence [content])))
 
 
 (defn log-on
-  [driver username password]
+  [^RemoteWebDriver driver username password]
   (let [user-field (.findElement driver (By/id "LoginForm:USER"))
         pass-field (.findElement driver (By/id "LoginForm:PASSWORD-blocked"))
         submit     (.findElement driver (By/id "LoginForm:submitInput"))]
@@ -28,7 +29,7 @@
   [start-page]
   (let [driver (doto (ChromeDriver.)
                  (.get start-page))]
-    ;; block until log on is possible
+    ;; block until login is possible
     (doto
       (WebDriverWait. driver 10)
       (.until (ExpectedConditions/elementToBeClickable (By/id "LoginForm:submitInput"))))
@@ -37,9 +38,14 @@
 
 ;; maybe not needed if the direct link is permanent
 (defn navigate-to-account
-  [driver account-link-text]
+  [^RemoteWebDriver driver account-link-text]
   (doto
     (WebDriverWait. driver 10)
     (.until (ExpectedConditions/elementToBeClickable (By/linkText account-link-text))))
-  (let [account-link (.findElement driver (By/linkText account-link-text))]
+  (let [^RemoteWebElement account-link (.findElement driver (By/linkText account-link-text))]
     (.click account-link)))
+
+
+(defn parse-account-table
+  [^RemoteWebElement table]
+  (.findElements table (By/tagName "tr")))
